7afe2027f2d38b93ce535bb7101c0e397ab364fb
Diego Jefferson <diegojfsr@gmail.com>
2022-08-22 20:17:47 -0300

    Exercicicos

---
+++ Exercicios/Comentarios.java
@@ -0,0 +1,23 @@
+package Exercicios;
+
+public class Comentarios {
+
+	public static void main(String[] args) {
+		
+		//Esse e um comentario de uma linha 2barras antes de escrever comenta a linha
+		
+		System.out.println("Ola Mundo!");
+		
+		/*
+		 * Esse é um
+		 * comentario de
+		 * varias
+		 * linhas.
+		 * todas as linhas ficam dentro de barra e asterisco
+		 */
+		
+		//Os comentarios nao afetam o funcionamento do codigo
+
+	}
+
+}
