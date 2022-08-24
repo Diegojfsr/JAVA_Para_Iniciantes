82bcfa33aef90f3fe2771cb08a5449937f285976
Diego Jefferson <diegojfsr@gmail.com>
2022-08-23 21:37:40 -0300

    Exercicios

---
+++ Exercicios/OperadoresMatematicos.java
@@ -0,0 +1,24 @@
+package Exercicios;
+
+public class OperadoresMatematicos {
+
+	public static void main(String[] args) {
+
+		int num1= 10;
+		int num2 = 5;
+		int num3 = 2;
+		
+		int soma = num1 + num2;
+		int subtracao = num1 - num2;
+		int multiplicacao = num2 * num3;
+		int divisao = num1 / num3;
+		int resto = num2 % num3;
+		
+		System.out.println(soma);
+		System.out.println(subtracao);
+		System.out.println(multiplicacao);
+		System.out.println(divisao);
+		System.out.println(resto);
+	}
+
+}
