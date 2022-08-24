afd2170c82e61cff531b221db388cff5fa3890c4
Diego Jefferson <diegojfsr@gmail.com>
2022-08-23 21:21:06 -0300

    Exercicios

---
+++ Exercicios/Variaveis.java
@@ -0,0 +1,20 @@
+package Exercicios;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		//tipo nome = conteudo;
+		String nome = "Diego Jefferson";
+		int idade = 29;
+		double altura = 1.73;
+		char sexo = 'M';
+		boolean casado = false;
+		
+		System.out.println(nome);
+		System.out.println(idade);
+		System.out.println(altura);
+		System.out.println(sexo);
+		System.out.println(casado);
+	}
+
+}
