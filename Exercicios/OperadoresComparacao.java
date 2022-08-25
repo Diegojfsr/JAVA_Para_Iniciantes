d4d9152cc476aca891cf348a36019dc433f2e81c
Diego Jefferson <diegojfsr@gmail.com>
2022-08-24 22:29:18 -0300

    Exercicios

---
+++ Exercicios/OperadoresComparacao.java
@@ -0,0 +1,21 @@
+package Exercicios;
+
+public class OperadoresComparacao {
+
+	public static void main(String[] args) {
+
+		boolean igual = 10==10;
+		boolean diferente = 10!=10;
+		boolean maior = 10>9;
+		boolean maiorouIgual = 10>=10;
+		boolean menor = 10<9;
+		boolean menorouIgual = 10<=10;
+		System.out.println(igual);
+		System.out.println(diferente);
+		System.out.println(maior);
+		System.out.println(maiorouIgual);
+		System.out.println(menor);
+		System.out.println(menorouIgual);
+	}
+
+}
