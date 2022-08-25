4a1e5de7128e3725b80dec0f4086c79838bb9137
Diego Jefferson <diegojfsr@gmail.com>
2022-08-25 20:51:54 -0300

    Exercicios

---
+++ Exercicios/EstruturaCondicionalIfElse.java
@@ -0,0 +1,16 @@
+package Exercicios;
+
+public class EstruturaCondicionalIfElse {
+
+	public static void main(String[] args) {
+		
+		int idade = 15;
+		if(idade>=18) {
+			System.out.println("Você é maior de 18 anos!");
+		}else {
+			System.out.println("Você é menor de 18 anos!");
+		}
+		
+	}
+
+}
