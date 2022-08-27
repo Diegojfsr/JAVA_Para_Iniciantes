3eaf799e595fd6c463d2fb56774e69be71393549
Diego Jefferson <diegojfsr@gmail.com>
2022-08-26 21:33:38 -0300

    Exercicios

---
+++ Exercicios/EstruturaRepeticaoWhile.java
@@ -0,0 +1,16 @@
+package Exercicios;
+
+public class EstruturaRepeticaoWhile {
+
+	public static void main(String[] args) {
+		//enquanto numero menor que 10 ele repete
+		int numero = 1;
+		while (numero<10) {
+			System.out.println(numero);
+			numero++;
+		}
+			
+			
+	}
+
+}
