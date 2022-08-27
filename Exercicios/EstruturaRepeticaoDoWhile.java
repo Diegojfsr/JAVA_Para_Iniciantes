7832bd9a2b2e69b2ba5e954e2a3733eccaebeeed
Diego Jefferson <diegojfsr@gmail.com>
2022-08-26 21:46:55 -0300

    Exercicios

---
+++ Exercicios/EstruturaRepeticaoDoWhile.java
@@ -0,0 +1,16 @@
+package Exercicios;
+
+public class EstruturaRepeticaoDoWhile {
+
+	public static void main(String[] args) {
+		//Faça o que esta entre chaves, 
+		//enquanto o que esta entre parenteses for verdadeiro
+		int numero = 1;
+		do {
+			System.out.println(numero);
+			numero++;
+		}while(numero<10);
+		
+	}
+
+}
