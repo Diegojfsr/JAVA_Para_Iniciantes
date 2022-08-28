e54c43a41d5404d180bdba11649359c9b49201ae
Diego Jefferson <diegojfsr@gmail.com>
2022-08-27 23:12:48 -0300

    Exercicios

---
+++ Exercicios/EstruturaRepeticaoFor.java
@@ -0,0 +1,14 @@
+package Exercicios;
+
+public class EstruturaRepeticaoFor {
+
+	public static void main(String[] args) {
+		//variavel recebe1
+		//ira repetir ate a variavel ser menor que 10
+		for (int i = 1; i<10; i++ ) {
+			System.out.println(i);
+		}
+		
+	}
+
+}
