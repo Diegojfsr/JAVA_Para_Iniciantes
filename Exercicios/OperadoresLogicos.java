2d985c2fecfafcfa1448f97493f9a08e101702f2
Diego Jefferson <diegojfsr@gmail.com>
2022-09-01 15:19:01 -0300

    Exercicios

---
+++ Exercicios/OperadoresLogicos.java
@@ -0,0 +1,45 @@
+package Exercicios;
+
+public class OperadoresLogicos {
+
+	public static void main(String[] args) {
+		//&& || ! (and, or, not)
+		
+	/*	int idade=18;
+		boolean temCarteira=true;
+		
+		if(idade>=18 && temCarteira == true) {//O && analiza se ambas as expressoes sao verdadeiras.
+			System.out.println("Pode dirigir!");
+		}else {
+			System.out.println("Não pode dirigir!");
+		}	
+	*/
+	/*	
+		int idade=18;
+		boolean temCarteira=false;
+		
+		if(idade>=18 || temCarteira == true) { //O or analiza se pelo menos uma das opções e verdadeira.
+			System.out.println("Pode dirigir!");
+		}else {
+			System.out.println("Não pode dirigir!");
+		}
+	*/	
+
+		if(true) {
+			System.out.println("Pode dirigir!");
+		}else {
+			System.out.println("Não pode dirigir!");
+		}
+		//-----------------------------------------
+		
+		if(!true) { //O ! nega a expressao
+			System.out.println("Pode dirigir!");
+		}else {
+			System.out.println("Não pode dirigir!");
+		}
+		
+		
+		
+	}
+
+}
