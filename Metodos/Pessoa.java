8744acb1dee76f7d41da10527444c682e7586c7f
Diego Jefferson <diegojfsr@gmail.com>
2022-09-08 11:04:50 -0300

    Exercicios

---
+++ Metodos/Pessoa.java
@@ -0,0 +1,16 @@
+package Metodos;
+
+public class Pessoa {
+	//Atributos são as caracteristicas da classe
+	//Metodos são as ações execultadas pela classe
+	
+	//Atributo nome
+	String nome;
+	
+	//Metodo dormir
+	//metodo de retorno void-vazio
+	void dormir() {
+		System.out.println("ZzZzZzZz");
+	}
+	
+}
---
+++ Metodos/Principal.java
@@ -0,0 +1,14 @@
+package Metodos;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+
+		Pessoa eu = new Pessoa();
+		//System.out.println(eu.nome);//enquanto nome estiver vazio ele sera null
+		//assessar o metodo dormir
+		eu.dormir();
+	}
+
+}
